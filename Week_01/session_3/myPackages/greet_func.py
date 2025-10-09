3066eaefe5c7cedb1c192c93786af5d0095de1d9
Rohit Gomes <gomesrohit92@gmail.com>
2025-10-09 14:25:51 +0530

    Week_1_session_3

---
+++ Week_01/session_3/myPackages/greet_func.py
@@ -0,0 +1,7 @@
+def say_hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+def say_goodbye(name: str) -> str:
+    return f"Goodbye, {name}!"
+
+greet_variable = "Hi this is greet_variable from greet_func module!"
